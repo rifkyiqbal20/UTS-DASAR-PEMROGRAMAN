9bf2675d16622c313d10912900a92430aa68d9f8
rifkyiqbal20 <55906180+rifkyiqbal20@users.noreply.github.com>
2019-10-29 16:19:19 +0700

    UTS RIFKY

---
+++ RIFKY/BAB II/Konversi Tipe Data.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char karakter = '*';
+    cout<<"Nilai ASCII"<< karakter
+        <<"Adalah "<< (int) karakter <<endl;
+
+    return 0;
+}
---
+++ RIFKY/BAB III/campur.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    char huruf;
+
+        huruf = 'A';        //nilai semula
+        cout<<"Isi Huruf Semula = "<< huruf <<endl;
+
+        huruf = huruf + 2; // ditambah dengan 2
+        cout<<"Isi Huruf Sekarang = "<< huruf <<endl;
+
+        return 0;
+
+}
